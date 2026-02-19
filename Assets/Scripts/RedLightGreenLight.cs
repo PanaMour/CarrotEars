d509ad1841e17e83fa1b80bf619650c8fcbc997d
PanaMour <panamour25@gmail.com>
2026-02-19 03:17:21 +0200

    Added Room 4

---
+++ Assets/Scripts/RedLightGreenLight.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+
+public class RedLightGreenLight : MonoBehaviour
+{
+    public CharacterController playerController;
+    public Transform failDestination;
+    public bool isRedLight = false;
+    public Light roomLight;
+    private float timer = 0f;
+    public float vel = 0.0000001f;
+    private bool playerInside = false;
+
+    void Update()
+    {
+        if (!playerInside)
+        {
+            return;
+        }
+
+        timer += Time.deltaTime;
+
+        if (timer > 3f)
+        {
+            isRedLight = !isRedLight;
+            timer = 0f;
+
+            if (roomLight != null)
+                roomLight.color = isRedLight ? Color.red : Color.green;
+        }
+
+        if (isRedLight && playerController.velocity.magnitude > vel)
+        {
+            TeleportPlayer();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInside = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInside = false;
+        }
+    }
+
+    void TeleportPlayer()
+    {
+        playerInside = false;
+        playerController.enabled = false;
+        playerController.transform.position = failDestination.position;
+        playerController.enabled = true;
+    }
+}
